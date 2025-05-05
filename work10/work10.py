272f83c3ace940af98d8f4e0fc7331812a024272
MrsNxstya <dusk.hexe@gmail.com>
2025-05-05 14:37:18 +0300

    Update work10.py

--- work10/work10.py
+++ work10/work10.py
@@ -13,6 +13,25 @@ file = open_file(file_name, "w")
 
 if file:
     file.write("Прізвище: Городецький\n")
-    file.write("Питання: Як називається  програмний пакет, який виконує код Python?\n")
+    file.write("Питання: Як називається програмний пакет, який виконує код Python?\n")
     file.close()
     print(f"Файл {file_name} закрито")
+
+file = open_file(file_name, "r")
+
+#Коваль А.М.
+if file:
+    lines = file.readlines()
+    file.close()
+
+    full_text = "".join(lines)
+    full_text += "Відповідь: Інтерпретатор Python (наприклад, CPython)\n"
+    full_text += "Прізвище: Коваль\n"
+
+    answer_file_name = "answer.txt"
+    answer_file = open_file(answer_file_name, "w")
+    if answer_file:
+        answer_file.write(full_text)
+        answer_file.close()
+        print(f"Результат записано у файл {answer_file_name}")
+
